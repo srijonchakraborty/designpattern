df5da05d2808da9196ccdaaccbad5a9b3fe4f637
srijonchakraborty <srijonchak@live.com>
2023-09-16 12:27:06 +0600

    Prototype Pattern implemented for stock class.

---
+++ DesignPattern/PrototypePattern/Contract/IPrototype.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace PrototypePattern.Contract
+{
+    public interface IPrototype<T> where T : class
+    {
+        T? ShallowClone(T current);
+        T? DeepClone(T current);
+        T? DeepUsingJsonClone(T current);
+    }
+}
---
+++ DesignPattern/PrototypePattern/Implementation/Stock/StockPrototype.cs
@@ -0,0 +1,57 @@
+﻿using Common.Model.Stock;
+using Newtonsoft.Json;
+using PrototypePattern.Contract;
+using System;
+using CMS=Common.Model.Stock;
+
+
+namespace PrototypePattern.Implementation.Stock
+{
+    public class StockPrototype : IPrototype<CMS.Stock>
+    {
+        public CMS.Stock? DeepClone(CMS.Stock current)
+        {
+            var response = new CMS.Stock()
+            {
+                Id = current.Id,
+                StockQuantity = current.StockQuantity,
+                Item = MapItem(current.Item),
+            };
+            return response;
+        }
+
+        private Product MapItem(Product? item)
+        {
+            Product product=new Product()
+            {
+                Id=item?.Id,
+                ItemName= item ?.ItemName,
+                Unit = item?.Unit 
+            };
+            return product;
+        }
+
+        public CMS.Stock? DeepUsingJsonClone(CMS.Stock current)
+        {
+            if (current != null)
+            {
+                string json = JsonConvert.SerializeObject(current);
+                var deserializedObject = JsonConvert.DeserializeObject<CMS.Stock>(json);
+                return deserializedObject;
+            }
+            else
+            {
+                return new CMS.Stock();
+            }
+        }
+        public CMS.Stock? ShallowClone(CMS.Stock current)
+        {
+            var response = new CMS.Stock() {
+                Id = current.Id,
+                StockQuantity= current.StockQuantity,
+                Item= current.Item,
+            };
+            return response;
+        }
+    }
+}
